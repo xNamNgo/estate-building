663934e918554546b8c1a13fbe5a3aca998a04bc
xxNamNgo <hoangnam11pdp@gmail.com>
2022-11-19 21:33:56 +0700

    del

--- src/main/java/com/laptrinhjavaweb/constant/BuildingConstant.java
+++
@@ -1,7 +0,0 @@
-package com.laptrinhjavaweb.constant;
-
-public class BuildingConstant {
-	public static final String WHERE_ONE_EQUAL_ONE =" where 1 = 1";
-	public static final String GROUP_BY_BUILDING_ID = " group by b.id";
-	public static final String SELECT_FROM_BULIDING = "select * from building as b";
-}
